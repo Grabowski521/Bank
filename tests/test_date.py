7ecad75166e7cd520e84410eb4aad1f9de532c9b
frol panasyuk <froli68f2016@yandex.ru>
2024-07-01 16:41:19 +0300

    test fixes

--- tests/test_date.py
+++ tests/test_date.py
@@ -5,4 +5,5 @@ def test_convert_date_valid(date_strings):
     assert convert_date(date_strings['valid']) == '11.07.2018'
 
 def test_convert_date_invalid(date_strings):
-    assert convert_date(date_strings['invalid']) == 'invalid'
+    with pytest.raises(ValueError):
+        convert_date(date_strings['invalid'])
