d0f56dc28f2bb445bd12cf3db230f9c38cb682d2
frol panasyuk <froli68f2016@yandex.ru>
2024-08-04 13:43:13 +0300

    decorators and their fixes

--- src/decorators.py
+++ src/decorators.py
@@ -2,12 +2,16 @@ from functools import wraps
 
 def log(filename = None):
     """
+    Функция-декоратор, которая регистрирует результат выполнения декорируемой функции.
     @log()
     def my_function(x, y):
 
     return x + y
 
     print(my_function(1, 2))
+    Декоратор log регистрирует результаты выполнения функции в файл или консоль. Если во время выполнения происходит
+    исключение, оно регистрируется вместе с входными аргументами.
+
     """
 
     def decorator(func):
@@ -34,9 +38,9 @@ def log(filename = None):
     return decorator
 
 @log()
-def my_function(x, y):
+@log("testlog.txt")
+def divide(a, b):
+    return a / b
 
-    return x + y
 
-
-print(my_function(1, 2))
+print(divide(1, 2))
--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,9 +1,28 @@
 import pytest
+import unittest
+import os
+from src.decorators import *
 
-from src.decorators import log, my_function
+class TestLogDecorator(unittest.TestCase):
 
+    def setUp(self):
+        # Удаляем файл логов перед каждым тестом, чтобы начать с чистого листа
+        if os.path.exists("testlog.txt"):
+            os.remove("testlog.txt")
 
-def test_log():
+    def test_successful_execution_logging(self):
+        result = divide(10, 2)
+        self.assertEqual(result, 5)  # Проверяем, что функция возвращает ожидаемый результат
+        with open("testlog.txt", "r") as file:
+            log_contents = file.read()
+            self.assertIn("divide ok", log_contents)  # Проверяем, что в логах есть запись об успешном выполнении
 
-    with pytest.raises(Exception):
-        my_function()
+    def test_exception_logging(self):
+        with self.assertRaises(ZeroDivisionError):
+            divide(10, 0)  # Вызываем функцию с некорректными аргументами, ожидаем исключение
+        with open("testlog.txt", "r") as file:
+            log_contents = file.read()
+            self.assertIn("divide error", log_contents)  # Проверяем, что в логах есть запись об ошибке
+
+if __name__ == "__main__":
+    unittest.main()
