0c3feff1213142623fe46124489d8148ffb5bbfb
frol panasyuk <froli68f2016@yandex.ru>
2024-12-15 23:06:24 +0300

    homework 12.2

--- logs/logging_config.py
+++ logs/logging_config.py
@@ -1,24 +1,39 @@
 import logging
 import os
 
-log_dir = 'logs'
-if not os.path.exists(log_dir):
-    os.makedirs(log_dir)
+# Создаем папку logs, если она не существует
+logs_dir = 'logs'
+if not os.path.exists(logs_dir):
+    os.makedirs(logs_dir)
 
-def setup_logger(name, log_file, level=logging.INFO):
-
+# Функция для настройки логирования
+def setup_logger(name, log_file, level=logging.DEBUG):
+    """Функция для настройки логера"""
+    # Создаем логер
     logger = logging.getLogger(name)
     logger.setLevel(level)
 
-    file_handler = logging.FileHandler(log_file, mode='w')
+    # Создаем обработчик для записи логов в файл
+    file_handler = logging.FileHandler(log_file, mode='w')  # 'w' для перезаписи логов при каждом запуске
     file_handler.setLevel(level)
 
+    # Создаем форматтер для логов
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     file_handler.setFormatter(formatter)
 
+    # Добавляем обработчик к логеру
     logger.addHandler(file_handler)
 
     return logger
 
-logger_masks = setup_logger('masks', os.path.join(log_dir, 'masks.log'))
-logger_utils = setup_logger('utils', os.path.join(log_dir, 'utils.log'))
+# Настройка логеров для модулей
+logger_masks = setup_logger('masks', os.path.join(logs_dir, 'masks.log'))
+logger_utils = setup_logger('utils', os.path.join(logs_dir, 'utils.log'))
+
+# Пример использования логеров
+if __name__ == "__main__":
+    logger_masks.info("Это информационное сообщение из модуля masks")
+    logger_masks.error("Это сообщение об ошибке из модуля masks")
+
+    logger_utils.debug("Это отладочное сообщение из модуля utils")
+    logger_utils.warning("Это предупреждающее сообщение из модуля utils")
