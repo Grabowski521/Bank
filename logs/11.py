900723fdcc0add9b36b0e7db32bd0ab054a5561a
frol panasyuk <froli68f2016@yandex.ru>
2024-12-15 21:24:27 +0300

    homework 12.2

--- logs/11.py
+++
@@ -1,5 +0,0 @@
-from logging_config import logger_utils
-
-def another_function():
-    logger_utils.info("Это информационное сообщение из модуля utils")
-    logger_utils.error("Это сообщение об ошибке из модуля utils")
